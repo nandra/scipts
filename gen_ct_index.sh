c8359effe491d155bb5cf5d118d051f8b306e816
Marek Belisko <marek.belisko@gmail.com>
2009-11-25 15:11:40 +0100

    Add script for generating cscope and ctags index files. Signed-off-by: Marek Belisko <marek.belisko@gmail.com>

---
+++ gen_ct_index.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Used for generating cscope and ctags files for
+# better code browsing
+
+DIR="$2"
+KERNEL="$1"
+C_DIR="$(pwd)"
+
+if [ "$DIR" = "" ];then
+    echo "Not correct path to project!!!"
+    echo "Usage:"
+    echo "gen_index.sh -k (-n) path_to_project"
+    echo "-k for kernel"
+    echo "-n for normal C project"
+    echo ""
+    echo "Quit"
+else
+    if [ "$DIR" = "." ];then
+	DIR="$(pwd)"
+	echo "dir=$DIR"
+    fi
+    cd $DIR
+    echo "Generating cscope database"
+    if [ "$KERNEL" = "-k" ];then
+        make cscope
+    elif [ "$KERNEL" = "-n" ];then
+	find $DIR -name '*.[ch]' > $DIR/cscope.files
+	cscope -b -q
+    else
+	echo "Please append correct option:"
+	echo "-k for kernel"
+	echo "-n for normal C project"
+	exit 0;
+    fi
+    echo "Generating ctags database"
+    ctags -R
+    cd $C_DIR
+fi
